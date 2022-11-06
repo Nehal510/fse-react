771ec2f13c6cc5b206ecf3239ca71a95572c8325
Nehal Bhatla <nehal@Nehals-MacBook-Pro.local>
2022-11-06 17:54:11 -0500

    A3

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -4,7 +4,8 @@ import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
 import axios from "axios";
 
-jest.mock('axios');
+//jest.mock('axios');
+const mock = jest.spyOn(axios, 'get');
 
 const MOCKED_USERS = [
   "alice", "bob", "charlie"
@@ -14,14 +15,58 @@ const MOCKED_TUITS = [
   "alice's tuit", "bob's tuit", "charlie's tuit"
 ];
 
+const MOCKED_TUITS_NEW = [
+    {
+        _id: "123",
+        tuit: "alice's tuit",
+        postedBy: "6352ac2bbf252f9a7d577d41",
+        postedOn: Date.now()
+    },
+    {
+        _id: "456",
+        tuit: "bob's tuit",
+        postedBy: "6359b7b57ead2919432e6373",
+        postedOn: Date.now()
+    },
+    {
+        _id: "789",
+        tuit: "charlie's tuit",
+        postedBy: "635ed6ca7ead2919432e63c7",
+        postedOn: Date.now()
+    }
+]
+
 test('tuit list renders static tuit array', () => {
-  // TODO: implement this
+  render(
+      <HashRouter>
+        <Tuits tuits={MOCKED_TUITS_NEW}/>
+      </HashRouter>);
+  const linkElement = screen.getByText(/bob's tuit/i);
+  expect(linkElement).toBeInTheDocument();
 });
 
 test('tuit list renders async', async () => {
-  // TODO: implement this
-})
+  mock.mockRestore();
+  const tuits = await findAllTuits();
+  render(
+      <HashRouter>
+        <Tuits tuits={tuits}/>
+      </HashRouter>);
+  const linkElement = screen.getByText(/tuit2/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test('tuit list renders mocked', async () => {
-  // TODO: implement this
+  axios.get.mockImplementation(() =>
+      Promise.resolve({ data: {tuits: MOCKED_TUITS_NEW} }));
+  const response = await findAllTuits();
+  const tuits = response.tuits;
+
+  render(
+      <HashRouter>
+        <Tuits tuits={tuits}/>
+      </HashRouter>);
+
+  const tuit = screen.getByText(/alice's tuit/i);
+  expect(tuit).toBeInTheDocument();
 });
--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,15 +1,83 @@
-describe('can create tuit with REST API', () => {
-  // TODO: implement this
+import {createTuit, deleteTuit, findAllTuits, findTuitById} from "../services/tuits-service";
+
+describe('createTuit', () => {
+    const newTuit = {
+        tuit: 'Hey, there!'
+    };
+    let createdTuit;
+    test('can create tuit with REST API', async () => {
+        createdTuit = await createTuit("6352ac2bbf252f9a7d577d41", newTuit);
+
+        expect(createdTuit.tuit).toEqual(newTuit.tuit);
+    });
+    afterAll(() => {
+        return deleteTuit(createdTuit._id);
+    });
 });
 
-describe('can delete tuit wtih REST API', () => {
-  // TODO: implement this
+describe('deleteTuitById', () => {
+    const newTuit = {
+        tuit: 'Hey, there!'
+    };
+    let createdTuit;
+    beforeAll(async() => {
+        createdTuit = await createTuit("6352ac2bbf252f9a7d577d41", newTuit);
+        return createdTuit;
+    });
+    test('can delete tuit with REST API', async () => {
+        const status = await deleteTuit(createdTuit._id);
+
+        expect(status.deletedCount).toBeGreaterThanOrEqual(1);
+    });
+    afterAll(() => {
+        return deleteTuit(createdTuit._id);
+    });
 });
 
-describe('can retrieve a tuit by their primary key with REST API', () => {
-  // TODO: implement this
+describe('findTuitById', () => {
+    const newTuit = {
+        tuit: 'Hey, there!'
+    };
+    let createdTuit;
+    test('can retrieve a tuit by their primary key with REST API', async () => {
+        createdTuit = await createTuit("6352ac2bbf252f9a7d577d41", newTuit);
+        expect(createdTuit.tuit).toEqual(newTuit.tuit);
+
+        const existingTuit = await findTuitById(createdTuit._id);
+        expect(existingTuit.tuit).toEqual(newTuit.tuit);
+    });
+    afterAll(() => {
+        return deleteTuit(createdTuit._id);
+    });
 });
 
-describe('can retrieve all tuits with REST API', () => {
-  // TODO: implement this
+describe('findAllTuits', () => {
+    const tuits = [
+        "Hello", "SpaceX is amazing", "Ice creams are love"
+    ]
+    let i = 0, newTuits;
+    newTuits = new Array(tuits.length);
+    beforeAll(async() => {
+      await Promise.all(tuits.map(async tuit => {
+          newTuits[i] = await createTuit("6352ac2bbf252f9a7d577d41", {tuit: tuit}).then(i=i+1)
+      }))
+    });
+    test('can retrieve all tuits with REST API', async () => {
+        const allTuits = await findAllTuits();
+        expect(allTuits.length).toBeGreaterThanOrEqual(tuits.length);
+
+        const tuitsWeInserted = allTuits.filter(
+            tuit => tuits.indexOf(tuit.tuit) >= 0);
+
+        // compare the actual users in database with the ones we sent
+        tuitsWeInserted.forEach(eachTuit => {
+            const tuitInserted = tuits.find(tuit => tuit === eachTuit.tuit);
+            expect(eachTuit.tuit).toEqual(tuitInserted);
+        });
+    });
+    afterAll(async() => {
+        await Promise.all(newTuits.map(async tuit => {
+            await deleteTuit(tuit._id);
+        }))
+    });
 });
